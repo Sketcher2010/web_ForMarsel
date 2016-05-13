d9c21045e0bcfe15da86f5c7ded81882653c15cd
Viktor <Snikerssurgut@gmail.com>
2016-05-13 19:59:04 +0300

    4 колонки, магия убрана

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var next_url = "";
 var created_time = "0";
 var isScrolled = false;
-var currentScrollId;
+var currentScrollId, totalCount = 0, countNow = 0;
 var interval = "";
 var vars = getUrlVars();
 $( document ).ready(function() {
@@ -33,11 +33,16 @@ function startScroll() {
     else
         isScrolled = true;
     interval = setInterval(function () {
-        if(currentScrollId == 0){
-            currentScrollId = $(".post").length-1;
+        currentScrollId = currentScrollId - 4;
+        if(currentScrollId <= 0){
+            currentScrollId = totalCount;
         }
-        currentScrollId--;
+        if(totalCount > countNow) {
+            currentScrollId = totalCount;
+            countNow = totalCount;
+        }
         $(".container").scrollTo($("#post"+currentScrollId), 500);
+        console.log(currentScrollId);
     }, 5000);
 }
 function resetScroll() {
@@ -45,13 +50,14 @@ function resetScroll() {
     currentScrollId = $(".post").length-1;
     $(".container").scrollTo($("#post"+currentScrollId), 500);
     clearInterval(interval);
+    console.log("clearing!");
     startScroll();
 }
 
 function getData(next_url, tag) {
     var config = {};
     config.Beget = {
-        apiKey: '1545509780.b63e58a.7f89fc42de1042e9b3797c88aa2286df',
+        apiKey: '1681582268.b63e58a.66c3da1c478747fd8fc518b913e73d08',
         apiHost: 'https://api.instagram.com/'
     };
     var res = "";
@@ -75,21 +81,25 @@ function getData(next_url, tag) {
                     created_time = obj.created_time;
                     var txt = obj.caption["text"];
                     $("#container").after(
-                        "<div id='post" + $(".post").length + "' class='post'><div id='marginPost"+$(".post").length+"'></div><div class='postContainer' id='postContainer" + $(".post").length + "'>" +
-                        "<img src='" + obj.images["standard_resolution"]["url"] + "' class='insta-photo'>" +
-                        "<div class=\"insta-author\">\
-                            <div class=\"left\">\
-                                <img src=\"" + obj.user["profile_picture"] + "\" width=\"40\" alt=\"\">\
-                                </div>\
-                                <div class=\"right\">\
-                                    <a href=\"" + obj.link + "\" target=\"_blank\">" + obj.user["username"] + "</a><br>\
-                                    <span class=\"like-count\">\
-                                    <span class=\"icon-heart\"></span>" + obj.likes["count"] + "</span>\
-                                </div>\
-                            <p>" + txt.replace(/(\#((.*?)[^\s]+))/gi, '<a href="?tag=$2" class="thatHashTag">$1</a>') + "</p>\
-                            </div></div>\
+                        "<div id='post" + $(".post").length + "' class='post'>" +
+                            "<div id='marginPost"+$(".post").length+"'></div>\
+                            <div id='postContainer"+$(".post").length+"' class='postContainer'>\
+                                <div class=\"insta-author\">\
+                                    <div class=\"left\">\
+                                        <img src=\"" + obj.user["profile_picture"] + "\" width=\"40\" alt=\"\">\
+                                    </div>\
+                                    <div class=\"right\">\
+                                         <a href=\"" + obj.link + "\" target=\"_blank\">" + obj.user["username"] + "</a><br>\
+                                         <span class=\"like-count\">\
+                                         <span class=\"icon-heart\"></span>" + obj.likes["count"] + "</span>\
+                                    </div>\
+                                 </div>\
+                                <img src='" + obj.images["low_resolution"]["url"] + "' class='insta-photo'>\
+                                <p>" + txt.replace(/(\#((.*?)[^\s]+))/gi, '<a href="?tag=$2" class="thatHashTag">$1</a>') + "</p>\
+                            </div>\
                         </div>");
-                    resetScroll();
+                        totalCount = $(".post").length;
+                    // resetScroll();
                 }
             }
             res = result.pagination["next_url"];
@@ -97,7 +107,7 @@ function getData(next_url, tag) {
                 startScroll();
             }
             for(var i = $(".post").length-1; i>=0; i--) {
-                $("#marginPost" + i).css({"height": (screen.height-260-$("#postContainer" + i).height())/2+"px"});
+                 $("#marginPost" + i).css({"height": (screen.height-260-$("#postContainer" + i).height())/2+"px"});
             }
             $(".post").css({"height": screen.height+"px"});
         },
